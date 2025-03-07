ab175471aa10ab1510689e9e900063a23192c330
Ho Thich An <htan02022003@gmail.com>
2025-03-07 19:16:46 +0700

    cap-nhat-binh-luan

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -10,29 +10,33 @@ class ReviewController extends Controller
 {
     public function comment()
     {
-        $comment = request('comment');
-        $rating = request('rating');
-        $product_id = request('product_id');
-        $data = [
-            'customer_id' => auth()->guard('customer')->user()->id,
-            'product_id' => $product_id,
-            'rating' => $rating,
-            'comment' => $comment,
-        ];
+        if (auth()->guard('customer')->check()) {
+            $comment = request('comment');
+            $rating = request('rating');
+            $product_id = request('product_id');
+            $data = [
+                'customer_id' => auth()->guard('customer')->user()->id,
+                'product_id' => $product_id,
+                'rating' => $rating,
+                'comment' => $comment,
+            ];
 
-        if (Evaluate::create($data)) {
-            $product = Product::where('id', $product_id)->first();
-            $averageRating = $product->averageRating;
-            $reviews = $product->evaluates()->paginate(5);
-            $countReviews = $product->evaluates()->get()->count();
-            $view = view('partials.comment', compact('reviews'))->render();
-            return response()->json([
-                'html' => $view,
-                'averageRating' => $averageRating,
-                'countReviews' => $countReviews
-            ], 200);
+            if (Evaluate::create($data)) {
+                $product = Product::where('id', $product_id)->first();
+                $averageRating = $product->averageRating;
+                $reviews = $product->evaluates()->paginate(5);
+                $countReviews = $product->evaluates()->get()->count();
+                $view = view('partials.comment', compact('reviews'))->render();
+                return response()->json([
+                    'html' => $view,
+                    'averageRating' => $averageRating,
+                    'countReviews' => $countReviews
+                ], 200);
+            } else {
+                return response()->json(['error' => 'Lỗi khi đánh giá'], 403);
+            }
         } else {
-            return response()->json(['error' => 'Lỗi khi đánh giá'], 403);
+            return response()->json(['error' => 'Bạn chưa đăng nhập'], 403);
         }
     }
 
